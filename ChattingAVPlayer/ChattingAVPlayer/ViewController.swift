0c729eff95775f3097645ce08c42938593cdd601
Flowergeoji <gyujae.kim@insertcoin.me>
2018-02-13 10:50:03 +0900

    first commit

---
+++ ChattingAVPlayer/ChattingAVPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChattingAVPlayer
+//
+//  Created by FlowerGeoji on 2018. 2. 13..
+//  Copyright © 2018년 FlowerGeoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
